b81191bb1e62400d0f56624c4a32cc6ea6bb1ae7
Jamie McElwain <jgmcelwain@icloud.com>
2019-09-12 17:44:10 +0100

    chore: initial commit

---
+++ dist/index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var vueheap = (function() {
+  'use strict';
+
+  var HEAP_INSTANCE_METHODS = [
+    'addEventProperties',
+    'addUserProperties',
+    'clearEventProperties',
+    'identify',
+    'resetIdentity',
+    'removeEventProperty',
+    'setEventProperties',
+    'track',
+    'unsetEventProperty'
+  ];
+  var VueHeap = {
+    createHeap: function createHeap(key, config) {
+      // initialize heap if necessary
+      window.heap = window.heap || []; // set configuration properties
+
+      window.heap.appid = key;
+      window.heap.config = config || {}; // while we are waiting for the heap script to load we still need to be
+      // able to capture any events that are meant to be logged by heap. once the
+      // script is loaded, these events will be uploaded to heap
+
+      var _loop = function _loop(i) {
+        window.heap[HEAP_INSTANCE_METHODS[i]] = function() {
+          if (window.heap && window.heap.push) {
+            window.heap.push([
+              HEAP_INSTANCE_METHODS[i],
+              Array.prototype.slice.call(arguments, 0)
+            ]);
+          }
+        };
+      };
+
+      for (var i = 0; i < HEAP_INSTANCE_METHODS.length; i++) {
+        _loop(i);
+      }
+    },
+    addHeapScript: function addHeapScript(key, config) {
+      var useHTTPS = config.forceSSL || 'https:' === document.location.protocol;
+      var heapScript = document.createElement('script');
+      heapScript.type = 'text/javascript';
+      heapScript.async = true;
+      heapScript.src = ''
+        .concat(
+          useHTTPS ? 'https:' : 'http:',
+          '//cdn.heapanalytics.com/js/heap-'
+        )
+        .concat(key, '.js');
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(heapScript, firstScriptTag);
+    },
+    install: function install(Vue, options) {
+      if (options.key === undefined) {
+        throw new Error('No Heap App ID specified.');
+      } // create a heap instance
+
+      this.createHeap(options.key, options);
+
+      if (options.enable) {
+        // load the heap script onto the page
+        this.addHeapScript(options.key, options);
+      } // add $heap reference so components can use this.$heap
+
+      Vue.prototype.$heap = window.heap;
+    }
+  };
+  return VueHeap;
+})();
